a6238df29aeba8a6893e9a93c9ae906dc0f6d54e
Louis Yu <louischrisyu@gmail.com>
2026-01-29 17:19:56 +0800

    add persistent list

--- src/main/java/Deadline.java
+++ src/main/java/Deadline.java
@@ -8,6 +8,15 @@ public class Deadline extends Task {
 	this.by = by;
     }
 
+    public String type() {
+	return "D";
+    }
+
+    @Override
+    public String getSaveData() {
+	return "D/" + this.getStatusIcon() + "/" + this.getName() + "/" + this.by;
+    }
+
     @Override
     public String printTask() {
 	return "[D]" + super.printTask() + " (by: " + this.by + ")";
